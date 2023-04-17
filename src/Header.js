c5b0d893c0b22eab90d8325a5e32a420d8e524c1
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-17 13:14:39 -0500

    add header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <>
+                <header>
+                    <h1>Horned Beasts</h1>
+                </header>
+            </>
+        )
+
+    }
+
+
+}
+
+export default Header;
