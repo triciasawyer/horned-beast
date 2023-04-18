2f1e0e792291ab0ca45473c2b9f376f102a69b60
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-18 11:43:24 -0500

    add functionality for clicking like

--- src/Beast.js
+++ src/Beast.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class Beast extends React.Component {
 
-    construcor(props) {
-        // likes = 0;
-    };
+    // construcor(props) {
+    //     super(props);
+    //     this.state = {
+    //     likes: 0,
+    //     };
+    // }
 
     // helper function 
     // handleLikes = () => {
@@ -21,8 +24,14 @@ class Beast extends React.Component {
             <>
                 <article>
                     <h2>Animal: {this.props.title}</h2>
+                    <p>{this.state.likes}Likes!</p>
+                    <p onClick={this.handleLikes}>Click to like this Animal</p>
                     <h3><img src={this.props.image_url} alt={this.props.title} /></h3>
                     <p>Description: {this.props.description}</p>
+
+
+
+                    <div></div>
                 </article>
             </>
         );
