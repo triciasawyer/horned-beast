41c5a867930677b948a3ce1352c8f966acf1dae3
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-21 10:23:37 -0500

    adding form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,103 @@ import data from './data.json';
 import SelectedBeast from './SelectedBeast.js';
 import Form from "react-bootstrap/Form";
 
+let menu = [this.state.horns];
+
+class HornForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manyHorns: ''
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    let manyHorns = event.target.manyHorns.value;
+    //add it to state
+
+    this.setState({
+      manyHorns: manyHorns,
+    });
+  };
+
+  handleInput = (event) => {
+    //we dont need to prevent default
+    let userName = event.target.value;
+    console.log("🚀 handleInput ~ userName", userName);
+    //now add to state
+    this.setState({
+      userName: userName,
+    });
+  };
+
+  handleSelect = (event) => {
+    let selected = event.target.value;
+    if (selected === "even") {
+      let newMenu = menu.filter((number) => number % 2 === 0);
+      this.setState({ sortedMenu: newMenu });
+    } else if (selected === "odd") {
+      //filter over our array of numbers
+      let newMenu = data.filter((number) => number % 2 !== 0);
+      //set state with the odds
+      this.setState({ sortedData: newMenu });
+    } else {
+      this.setState({ sortedData: data });
+    }
+  };
+
+
+
+
+
+
+
+
+  render() {
+    let horns = this.state.horns.map((number, index) => {
+      return <ListGroup.Item key={index}>{number} [index~] {this.state.horns[index]}</ListGroup.Item>;
+    });
+
+    return (
+      <>
+        <section className="myLayout">
+          <header>Forms in React</header>
+          <main>
+            <section className="listHorns">
+              <ListGroup>{horns}</ListGroup>
+            </section>
+
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Label>
+                Name:
+                <input type="text" name="userName" onInput={this.handleInput} />
+              </Form.Label>
+
+
+
+              <Form.Group controlId="Horns">
+                <Form.Label>Horns:</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+
+
+
+              <p>Selected Horns</p>
+              <Form.Select name="selected" onChange={this.handleSelect}>
+                <option value="all">All</option>
+                <option value="even">Even</option>
+                <option value="odd">Odd</option>
+              </Form.Select>
+
+              <Button type="submit">Submit</Button>
+            </Form>
+          </main>
+        </section>
+      </>
+    );
+  }
+}
 
 
 
